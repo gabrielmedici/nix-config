c01b0878a81b2a85491e4600aa5d7c298c4747cc
Gabriel Medici <j.gabriel.medici@gmail.com>
2025-04-06 13:18:16 -0300

    added the nix-env-selector extension to vscode for convenience

--- modules/vscode.nix
+++ modules/vscode.nix
@@ -8,6 +8,8 @@
         (vscode-with-extensions.override {
             vscodeExtensions = with vscode-extensions; [
                 jnoortheen.nix-ide
+                arrterian.nix-env-selector
+                
                 ms-python.python
                 rust-lang.rust-analyzer
             ];
